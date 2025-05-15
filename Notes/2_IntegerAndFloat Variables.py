65eb317e74441b35fea743aef697659dca620651
codeSavvy-ln <codeSavvy.learn@gmail.com>
2025-05-16 00:43:53 +0530

    05_16

--- Notes/2_IntegerAndFloat Variables.py
+++ Notes/2_IntegerAndFloat Variables.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Integer is a number which is not included under quotation mark.
 #Float is a number with decimal points.
 
@@ -45,53 +44,3 @@ integer_variable = 5
 integer_variable_casting_to_string = str(integer_variable)
 
 print(type(integer_variable_casting_to_string))
-
-=======
-#Integer is a number which is not included under quotation mark.
-#Float is a number with decimal points.
-
-Numeric_value = 10
-print("Numeric value is : "+str(Numeric_value))
-
-#Using Arithmatic Operators
-
-First_Value = 3.1
-Second_Value = 5.2
-
-# ADDITION
-
-#Addition_Result = First_Value + Second_Value
-print("Addition :"+str(First_Value + Second_Value))
-
-# MULTIPLICATION
-
-Multiply_Result = First_Value * Second_Value
-print("Multiplication :"+str(Multiply_Result))
-
-# DIVIDE
-
-Divide_Result = First_Value / Second_Value
-print("Divide Result :" +str(Divide_Result))
-
-#Practice
-
-Gross_Salary = 10000
-Health_Insurance = 430.99
-House_rent = 1200
-Food = 400.5
-Salary_Tax = 200
-Donations_Poor = 100
-
-Net_Salary = Gross_Salary - (Health_Insurance+House_rent+Food+Salary_Tax+Donations_Poor)
-print("Net Salary : "+str(Net_Salary))
-
-# Variable Casting
-
-print("Casting from integer to string")
-
-integer_variable = 5
-integer_variable_casting_to_string = str(integer_variable)
-
-print(type(integer_variable_casting_to_string))
-
->>>>>>> 77a7026290cffc14ad5280317110e1c4e2e6e29e
