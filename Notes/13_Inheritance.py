20d01138686127ace470106293f45a867923e3c9
codeSavvy-ln <codeSavvy.learn@gmail.com>
2025-05-17 18:00:54 +0530

    f

--- Notes/13_Inheritance.py
+++ Notes/13_Inheritance.py
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 """
 - Inheritance is a subtopic in OOP
 - Inheritance allow us to define a new class that will inherit all the properties and methods from another class.
 - It allows us to have a parent  child relationship between classes
     parent class (base class)- class being inherited from
     child class - Class which will inherit properties and methods from a parent class
-=======
-"""
-- Inheritance is a subtopic in OOP
-- Inheritance allow us to define a new class that will inherit all the properties and methods from another class.
-- It allows us to have a parent  child relationship between classes
-    parent class (base class)- class being inherited from
-    child class - Class which will inherit properties and methods from a parent class
->>>>>>> 77a7026290cffc14ad5280317110e1c4e2e6e29e
-"""
