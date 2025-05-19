4d4894add9a453b86877be2994451cf00a3586cb
codeSavvy-ln <codeSavvy.learn@gmail.com>
2025-05-19 22:04:51 +0530

    cc

--- Notes/Tricks.py
+++ Notes/Tricks.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 1. Ctrl+Lft_Shift + F7 = Highlight text = (First choose the line which you want to highlight)
 2. Use () bracket when using any command like .get() or .insert() otherwise use []
@@ -14,21 +13,4 @@ quantity = 3
 itemno = 567
 price = 49.95
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
-=======
-"""
-1. Ctrl+Lft_Shift + F7 = Highlight text = (First choose the line which you want to highlight)
-2. Use () bracket when using any command like .get() or .insert() otherwise use []
-3. print("\n") to give space between the two print commands in the output
-4. Arguments are basically data like variables.
-5. Keyword arguments comes in the form of 'Key = Value' syntax (familiar from dictionaries)
-6. ctrl+alt+shift+insert = creates a new script that does not need a project to save it.
-
-
-"""
-
-quantity = 3
-itemno = 567
-price = 49.95
-myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
->>>>>>> 77a7026290cffc14ad5280317110e1c4e2e6e29e
 print(myorder.format(quantity, itemno, price))
