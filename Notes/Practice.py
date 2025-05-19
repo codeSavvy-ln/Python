6b59417ae0c00e8293cdce9681bbb4588eced120
codeSavvy-ln <codeSavvy.learn@gmail.com>
2025-05-19 22:03:40 +0530

    ccc

--- Notes/Practice.py
+++ Notes/Practice.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """def is_leap(year):
     leap = False  # Default assumption: not a leap year
 
@@ -28,36 +27,3 @@ coordinates = [[i, j, k]
                if i + j + k != n]
 
 print(coordinates)
-
-=======
-"""def is_leap(year):
-    leap = False  # Default assumption: not a leap year
-
-    # Leap year logic
-    if year % 4 == 0:  # Divisible by 4
-        if year % 100 == 0:  # Divisible by 100
-            if year % 400 == 0:  # Divisible by 400
-                leap = True
-        else:
-            leap = True
-
-    return leap  # Return True if leap year, False otherwise
-
-year = int(input("Enter a year: "))
-print(is_leap(year))
-"""
-
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-coordinates = [[i, j, k]
-               for i in range(x + 1)
-               for j in range(y + 1)
-               for k in range(z+1)
-               if i + j + k != n]
-
-print(coordinates)
-
->>>>>>> 77a7026290cffc14ad5280317110e1c4e2e6e29e
